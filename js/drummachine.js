01a17b05db0a502bc770d19935f78af1d77e93b5
zhagan <zhagan@gmail.com>
2017-12-14 17:58:30 -0700

    updated visualize

--- js/drummachine.js
+++ js/drummachine.js
@@ -1,11 +1,13 @@
 
+ $(document).ready(function() {
         //initialize buttons
-        function initUI(){
 
+        function initUI(){
+          //drum matrix ui
               let trkLength = 16;
               let trkContainer = $('<div>');
             for(var t = 0; t<4;t++){
-                let track = $('<div>');
+                let track = $('<div class="tracks">');
                 track.attr('id',"track"+(t+1));
                 let trkBtnArray = [];
 
@@ -20,24 +22,28 @@
 
                   }
                   track.append(trkBtnArray);
-                  track.prepend("track "+(t+1));
+                  track.prepend(t+1);
                   trkContainer.append(track);
                 }
 
            $('#trkContainer').append(trkContainer);
 
-
+           //instrument controls UI
            let trackUI = $('<div id="trackUI">');
 
            for(var i = 0; i<4;i++){
 
                   let instDiv = $('<div id="instDiv">');
-                  let sliderDiv = $('<div id="sliderDiv">');
+                  let sliderDiv = $('<div id="sliderDiv" class="sldr">');
                   //instDiv.html("inst");
                   //instDiv.attr('id',"inst"+i);
-                  instDiv.append(`<h2>${i+1}</h2>`);
-                  sliderDiv.slider();
+                  instDiv.append(`<h3>${i+1}</h3>`);
+                  sliderDiv.slider({
+                        value: 70,
 
+                  });
+                  sliderDiv.attr('track',i+1);
+                  instDiv.append(`<p id="volLbl">Volume</p2>`);
                   instDiv.append(sliderDiv);
                   trackUI.append(instDiv);
 
@@ -117,7 +123,7 @@
               if(sequence[x][parseFloat(args)] == 1){
                 var trigger = 'noteTrigger'+x;
                 Pd.send(trigger,[1]);
-                console.log('ping: '+trigger);
+                //console.log('ping: '+trigger);
               }
           }
           //automate the button background color to refelct tick number
@@ -175,12 +181,12 @@
                 if(sequence[0][parseFloat(value)-1]==0){
                 sequence[0][parseFloat(value)-1]=1;
                 //console.log('button pressed', value);
-                track1_btns[value].style.background='#00FF00'
+                track1_btns[value-1].style.background='#00FF00'
                 }else{
                 sequence[0][parseFloat(value)-1]=0;
                 //console.log('button pressed', value);
-                track1_btns[value].style.background='#000000'
-                }
+                track1_btns[value-1].style.background='white'
+                  }
             });
 
             var track2_btns = $(".track2");
@@ -192,9 +198,9 @@
                   //console.log('button pressed', value);
                   track2_btns[value-1].style.background='#00FF00'
                 }else{
-                sequence[1][parseFloat(value)-1]=0;
-                //console.log('button pressed', value);
-                track2_btns[value-1].style.background='#000000'
+                  sequence[1][parseFloat(value)-1]=0;
+                  //console.log('button pressed', value);
+                  track2_btns[value-1].style.background='white'
                 }
             });
 
@@ -207,9 +213,9 @@
                   //console.log('button pressed', value);
                   track3_btns[value-1].style.background='#00FF00'
                 }else{
-                sequence[2][parseFloat(value)-1]=0;
-                //console.log('button pressed', value);
-                track3_btns[value-1].style.background='#000000'
+                  sequence[2][parseFloat(value)-1]=0;
+                  //console.log('button pressed', value);
+                  track3_btns[value-1].style.background='white'
                 }
             });
 
@@ -224,7 +230,7 @@
                 }else{
                   sequence[3][parseFloat(value)-1]=0;
                   //console.log('button pressed', value);
-                  track4_btns[value-1].style.background='#000000'
+                  track4_btns[value-1].style.background='white'
                 }
             });
         }());
@@ -246,10 +252,11 @@
 
 
         //the volume slider function
-        $( "#slider" ).slider({
+        $( '.sldr' ).slider({
           change: function( event, ui ) {
-            console.log('slider '+ui.value);
-            Pd.send('volume', [ui.value]);
+            // console.log('slider '+ui.value);
+            // console.log('track'+$(this).attr('track'));
+            Pd.send($(this).attr('track')+'-volume', [ui.value]);
           }
           });
 
@@ -258,13 +265,13 @@
 //begin analyzer code
         var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
         ///var audioCtx = Pd.getAudio();
-        var analyser = audioCtx.createAnalyser();
+      //  var analyser1; // = audioCtx.createAnalyser();
         //var audioCtx2 = ;
-        var analyser2;
+      //  var analyser2;
 
-        analyser.minDecibels = -90;
-        analyser.maxDecibels = -10;
-        analyser.smoothingTimeConstant = 0.85;
+        // analyser.minDecibels = -90;
+        // analyser.maxDecibels = -10;
+        // analyser.smoothingTimeConstant = 0.85;
 
         if (navigator.getUserMedia) {
           console.log('getUserMedia supported.');
@@ -277,15 +284,39 @@
               // Success callback
               function(stream) {
                  source = audioCtx.createMediaStreamSource(stream);
-                 gain = Pd.getAudio().context.createGain();
+
+                 gain1 = Pd.getAudio().context.createGain();
+                 gain2 = Pd.getAudio().context.createGain();
+                 gain3 = Pd.getAudio().context.createGain();
+                 gain4 = Pd.getAudio().context.createGain();
+
+                 analyser1 = Pd.getAudio().context.createAnalyser();
+                 analyser1.minDecibels = -110;
 
                  analyser2 = Pd.getAudio().context.createAnalyser();
                  analyser2.minDecibels = -110;
+
+                 analyser3 = Pd.getAudio().context.createAnalyser();
+                 analyser3.minDecibels = -110;
+
+                 analyser4 = Pd.getAudio().context.createAnalyser();
+                 analyser4.minDecibels = -110;
                 // analyser2.maxDecibels = -10;
                 // analyser2.smoothingTimeConstant = 0.85;
 
-                 patch.o(0).getOutNode().connect(gain);
-                 gain.connect(analyser2);
+
+                //where and int .o(x) indicates which outlet~ from pd
+                 patch.o(0).getOutNode().connect(gain1);
+                 gain1.connect(analyser1);
+
+                 patch.o(1).getOutNode().connect(gain2);
+                 gain2.connect(analyser2);
+
+                 patch.o(2).getOutNode().connect(gain3);
+                 gain3.connect(analyser3);
+
+                 patch.o(3).getOutNode().connect(gain4);
+                 gain4.connect(analyser4);
 
                 // source.connect(analyser);
                 // analyser.connect(distortion);
@@ -294,8 +325,30 @@
                 // convolver.connect(gainNode);
                 // gainNode.connect(audioCtx.destination);
 
-                 visualize();
-                // voiceChange();
+                //moved from visualize
+                var canvas =  document.querySelector('.visualizer');
+                var canvasCtx = canvas.getContext("2d");
+                var intendedWidth = document.querySelector('.wrapper').clientWidth;
+
+                 visualize(canvas, canvasCtx, intendedWidth, analyser1);
+
+                 var canvas1 =  document.querySelector('.visualizer1');
+                 var canvasCtx1 = canvas1.getContext("2d");
+                 var intendedWidth1 = document.querySelector('.wrapper1').clientWidth;
+
+                 visualize(canvas1, canvasCtx1, intendedWidth1, analyser2);
+
+                 var canvas2 =  document.querySelector('.visualizer2');
+                 var canvasCtx2 = canvas2.getContext("2d");
+                 var intendedWidth2 = document.querySelector('.wrapper2').clientWidth;
+
+                  visualize(canvas2, canvasCtx2, intendedWidth2, analyser3);
+
+                  var canvas3 =  document.querySelector('.visualizer3');
+                  var canvasCtx3 = canvas3.getContext("2d");
+                  var intendedWidth3 = document.querySelector('.wrapper3').clientWidth;
+
+                  visualize(canvas3, canvasCtx3, intendedWidth3, analyser4);
 
               },
 
@@ -309,23 +362,20 @@
               }
 
 
-  function visualize() {
-        var WIDTH = 200;
+  function visualize(canvas, canvasCtx, intendedWidth, analyser) {
+        var WIDTH = 100;
         var HEIGHT = 100;
 
         //let canvas = $('<canvas class="visualizer">');
         //$('#header-title').append(canvasDiv);
-        var canvas =  document.querySelector('.visualizer');
-        var canvasCtx = canvas.getContext("2d");
 
-        var intendedWidth = document.querySelector('.wrapper').clientWidth;
 
         canvas.setAttribute('width',intendedWidth);
 
         var drawVisual;
 
-        analyser2.fftSize = 2048;
-        var bufferLength = analyser2.fftSize;
+        analyser.fftSize = 2048;
+        var bufferLength = analyser.fftSize;
         console.log(bufferLength);
         var dataArray = new Uint8Array(bufferLength);
 
@@ -335,7 +385,7 @@
 
           drawVisual = requestAnimationFrame(draw);
           //dataArray = Pd.getAudio().getContext;
-          analyser2.getByteTimeDomainData(dataArray);
+          analyser.getByteTimeDomainData(dataArray);
 
           canvasCtx.fillStyle = 'rgb(200, 200, 200)';
           canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
@@ -366,8 +416,9 @@
           canvasCtx.stroke();
         };
 
-
-
     draw();
 
- }
+ }//end visualize
+
+
+ }); //end on ready
